f52df3adb1ffc3e2090a75ac05232c144a8db3d9
carlos <cmtrfenix@gmail.com>
2019-12-04 23:40:18 -0500

    aplicacion eliminacion de archivo .json

--- app/routes/admin/login.py
+++ app/routes/admin/login.py
@@ -1,12 +1,6 @@
 from app import app, render_template
 import json
-f =open("./app/config/config.json","r")
-file=f.read()
-CONFIG = json.loads(file)
 
-#app.config.from_object("config.DevelopmentConfig") 
-URLBASE = CONFIG['DEFAULT']['URLBASE']
 @app.route("/login")
 def login():
-    urlrev = URLBASE 
-    return render_template("login.html", url = urlrev)
+    return render_template("login.html")
