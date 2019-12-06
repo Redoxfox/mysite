debc13f4082339990dd7efdd599ff57931f5419c
carlos <cmtrfenix@gmail.com>
2019-12-05 23:39:59 -0500

    Enrutar archivos configuracion

---
+++ app/model/modeldb.py
@@ -0,0 +1,16 @@
+from app import app, render_template
+import os
+import json
+dir_act = os.getcwd()
+route_file_config = dir_act 
+route_exist = route_file_config.find("mysite")
+if route_exist > 0:
+    route_file_config = dir_act + "/app/config/config.json"
+else:
+    route_file_config = dir_act + "/mysite/app/config/config.json"
+
+f =open(route_file_config,"r")
+file=f.read()
+CONFIG = json.loads(file)
+URLBASE = CONFIG['DEFAULT']['URLBASE']
+
