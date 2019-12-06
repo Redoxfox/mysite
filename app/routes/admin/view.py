1c84037c28aa679049a7b078d6a84909380bda05
carlos <cmtrfenix@gmail.com>
2019-12-05 23:12:00 -0500

    Enrutar archivos configuracion

--- app/routes/admin/view.py
+++ app/routes/admin/view.py
@@ -1,8 +1,21 @@
 from app import app, render_template
+import os
+import json
+dir_act = os.getcwd()
+route_file_config = dir_act 
+#+ "/app/config/config.json"
+route_exist = route_file_config.find("mysite")
+if route_exist > 0:
+    route_file_config = dir_act + "/app/config/config.json"
+else:
+    route_file_config = dir_act + "mysite/app/config/config.json"
 
+print(dir_act)
+print(os.path.isfile(route_file_config))
+print(os.path.isdir('app/config'))
 
 @app.route("/")
 def index():
     Sql="ddasddf"
-    lista ="lista"
+    lista = route_file_config 
     return render_template("index.html", sql=Sql, lista=lista)
