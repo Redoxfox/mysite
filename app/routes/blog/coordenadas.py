8a12c19bbd0bcd30969e832ee60f1ccbe51526a7
carlos <cmtrfenix@gmail.com>
2020-06-25 00:26:09 -0500

    Simulacion consumo API

--- app/routes/blog/coordenadas.py
+++ app/routes/blog/coordenadas.py
@@ -14,18 +14,33 @@ def coordenadas():
 @app.route("/blog/ver_coordenadas/", methods=["POST"])
 def ver_coordenadas():
     req = request.get_json() #Conversion de datos tipos json a lenguaje python (diccionario/lista)
-    #print(req)
+    print(req)
     lista_coordenadas = req["coordenadas"] #Extraer los datos como lista python
-    #print(lista_coordenadas)
-    #for item in lista_coordenadas:
-    #    print(item)
-    
-
+    print(lista_coordenadas)
+    for item in lista_coordenadas:
+        print(lista_coordenadas[0])
+     
     #Diccionario con lista de coordenadas se enviara a la vista    
     result = {"array":lista_coordenadas}
     #Respuesta de exito de peticion.
+    #No se como tienes los datos de coordenadas almacenadas pero un ejemplo de modelo de objeto
+    # que puede entender json es este. Son varios diccionarios con latitud y longitud y estos dentro de una lista
+    # algo asi similar te deberia devolver una consulta de la base de datos y ese array lo guardas en un diccionario
+    # con clave coordenadas. Yo no tengo base de datos asi que solo simulo la lectura con datos traidos desde la vista
+    """  {'coordenadas': [{'lat': 4.443581, 'lng': -75.230992}, 
+    {'lat': 4.445581, 'lng': -75.231992}, 
+    {'lat': 4.446581, 'lng': -75.232992}, 
+    {'lat': 4.447581, 'lng': -75.233992}, 
+    {'lat': 4.448581, 'lng': -75.234992}, 
+    {'lat': 4.449581, 'lng': -75.235992}, 
+    {'lat': 4.440581, 'lng': -75.236992}, 
+    {'lat': 4.441581, 'lng': -75.237992}, 
+    {'lat': 4.442581, 'lng': -75.238992}, 
+    {'lat': 4.447581, 'lng': -75.239992}]
+    } """
     res = make_response(jsonify({"message": "OK"}), 200)
     
     """Nota: Los print y el for comentados permiten ver los datos en consola pero como se muestran en 
        servidor WEB no se pueden ver a menos que se implemente desde cero la APP"""
     return result
+
--- app/static/js/js_homeblg/api.js
+++ app/static/js/js_homeblg/api.js
@@ -1,16 +1,45 @@
 function api(){
+    // Heatmap data
+    function getPoints(LatLng) {
+            
+      const results = [];  
+      for (let index = 0; index < LatLng.length; index++) {
+        const element = LatLng[index];
+        console.log(element[0],element[1])
+        results.push("new google.maps.LatLng(" + element[0] +","+ element[1]+")");
+      }
+      return results; //Este array es la simulacion de lo que retorna la funcion original
+     
+
+      /*No pude tener acceso a la API por tal motivo la funcion new google.maps.LatLng(4.443581, -75.231992) no
+      la reconoce mi codigo y me saca error pero trate de emular eso con un array que recibe los valores*/
+
+        /*     return [
+                new google.maps.LatLng(4.443581, -75.231992),
+                new google.maps.LatLng(4.443884, -75.231993),
+                new google.maps.LatLng(4.443887, -75.231994),
+                new google.maps.LatLng(4.443890, -75.231995),
+                new google.maps.LatLng(4.443893, -75.231996),
+                new google.maps.LatLng(4.443896, -75.231997),
+                new google.maps.LatLng(4.443604, -75.231996),
+                new google.maps.LatLng(4.443750, -75.231998),
+                new google.maps.LatLng(4.443740, -75.231685),
+                new google.maps.LatLng(4.443738, -75.231680),
+            ]; */
+    }
+
     var entry = {
         coordenadas: [
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)",
-            "new google.maps.LatLng(4.443581, -75.231992)"
+            {lat:4.443581, lng:-75.230992},
+            {lat:4.445581, lng:-75.231992},
+            {lat:4.446581, lng:-75.232992},
+            {lat:4.447581, lng:-75.233992},
+            {lat:4.448581, lng:-75.234992},
+            {lat:4.449581, lng:-75.235992},
+            {lat:4.440581, lng:-75.236992},
+            {lat:4.441581, lng:-75.237992},
+            {lat:4.442581, lng:-75.238992},
+            {lat:4.447581, lng:-75.239992}
         ]
       };
       url = window.origin + "/blog/ver_coordenadas/"
@@ -33,12 +62,32 @@ function api(){
             console.log(data.array)
             let coodenadas = document.getElementById("coodenadas");
             coodenadas_array = data.array
-            let newarray = coodenadas_array.map(num_coordenada)
-            function num_coordenada(item) {
-                mis_item = "<li>" + item + "</li>"
-                return mis_item;
-              }
-              coodenadas.innerHTML = ` ${newarray} <br> `
+
+            LatLng = coodenadas_array.map(num_coordenada)
+              
+            function num_coordenada (value){
+              coord = []
+              lat = value.lat
+              lng = value.lng
+              coord.push(lat)
+              coord.push(lng)
+              
+              return coord;
+            }
+            
+            console.log(getPoints(LatLng))//abre el inspector de elementos en el navegador cuando ejecutes y 
+            //podras ver el array con los valores de las coordenadas.
+
+            for (let index = 0; index < LatLng.length; index++) {
+              const element = LatLng[index];
+              let Extra = document.createElement('div');
+              let id_Extra  = "Extra" + index
+              Extra.setAttribute("id", id_Extra);
+              Extra.textContent = "latitud:"+element[0]+"---"+"longitud:"+element[1]
+              coodenadas.appendChild(Extra);
+            }
+            
+           
           });
         })
         .catch(function (error) {
