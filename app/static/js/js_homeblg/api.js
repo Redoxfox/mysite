86af41200abd88a59c4eb87df6483258d832b5b9
carlos <cmtrfenix@gmail.com>
2020-07-07 00:09:04 -0500

    Geo-localizacion departamentos Colombia

--- app/static/js/js_homeblg/api.js
+++ app/static/js/js_homeblg/api.js
@@ -30,16 +30,38 @@ function api(){
 
     var entry = {
         coordenadas: [
-            {lat:4.443581, lng:-75.230992},
-            {lat:4.445581, lng:-75.231992},
-            {lat:4.446581, lng:-75.232992},
-            {lat:4.447581, lng:-75.233992},
-            {lat:4.448581, lng:-75.234992},
-            {lat:4.449581, lng:-75.235992},
-            {lat:4.440581, lng:-75.236992},
-            {lat:4.441581, lng:-75.237992},
-            {lat:4.442581, lng:-75.238992},
-            {lat:4.447581, lng:-75.239992}
+          {lat:-1.0197222,	lng:-71.9383333},
+          {lat:7,	lng:-75.5},
+          {lat:7.0902778,	lng:-70.7616667},
+          {lat:10.75,	lng:-75},
+          {lat:9,	lng:-74.3333333},
+          {lat:5.5,	lng:-72.5},
+          {lat:5.25	,lng:-75.5},
+          {lat:1,	lng:-74},
+          {lat:5.5,	lng:-71.5},
+          {lat:2.5,	lng:-76.8333333},
+          {lat:9.3333333,	lng:-73.5},
+          {lat:6,	lng:-77},
+          {lat:5,	lng:-74.1666667},
+          {lat:8.3333333,	lng:-75.6666667},
+          {lat:2.5,	lng:-69},
+          {lat:1.6894444,	lng:-72.8202778},
+          {lat:2.5,	lng:-75.75},
+          {lat:11.5,	lng:-72.5},
+          {lat:10,	lng:-74.5},
+          {lat:3.5,	lng:-73},
+          {lat:1.5,	lng:-78},
+          {lat:8,	lng:-73},
+          {lat:0.5,	lng:-76},
+          {lat:4.5,	lng:-75.6666667},
+          {lat:5,	lng:-76},
+          {lat:12.5847222,	lng:-81.7005556},
+          {lat:7,	lng:-73.25},
+          {lat:9,	lng:-75},
+          {lat:3.75,	lng:-75.25},
+          {lat:3.75,	lng:-76.5},
+          {lat:0.25,	lng:-70.75},
+          {lat:5,	lng:-69.5}
         ]
       };
       url = window.origin + "/blog/ver_coordenadas/"
@@ -78,6 +100,18 @@ function api(){
             console.log(getPoints(LatLng))//abre el inspector de elementos en el navegador cuando ejecutes y 
             //podras ver el array con los valores de las coordenadas.
 
+            function dibujar(lat, long){
+              
+              L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+  
+                L.marker([lat, long]).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+              
+            }
+            var map = L.map('map').setView([7, -75.5], 6);
             for (let index = 0; index < LatLng.length; index++) {
               const element = LatLng[index];
               let Extra = document.createElement('div');
@@ -85,13 +119,15 @@ function api(){
               Extra.setAttribute("id", id_Extra);
               Extra.textContent = "latitud:"+element[0]+"---"+"longitud:"+element[1]
               coodenadas.appendChild(Extra);
-            }
-            
-           
+             
+              dibujar(element[0], element[1])
+              }
           });
         })
         .catch(function (error) {
           console.log("Fetch error: " + error);
         });
 }
+
+
 
